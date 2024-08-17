ac926c6df6ca4f11f0a1a09f46017155a92ec14b
thanaphon <e21bvz@gmail.com>
2024-08-18 06:19:58 +0700

    update: ui-size and color

--- calculate-app/src/app/utils/utility.ts
+++ calculate-app/src/app/utils/utility.ts
@@ -1,37 +1,37 @@
-import { ChangeEvent } from "react";
+// import { ChangeEvent } from "react";
 
-export const validateInterestRate = (value: string) => {
-    if (!/^[1-9]\d?(\.\d{1,2})?$/.test(value)) {
-        setErrors((prev) => ({
-            ...prev,
-            interestRate: "Invalid interest rate format",
-        }));
-    } else {
-        setErrors((prev) => {
-            const { interestRate, ...rest } = prev;
-            return rest;
-        });
-    }
-};
-
-
-
-export const validatePrice = (value: string) => {
-    if (!/^[1-9][0-9]{0,11}$/.test(value.replace(/,/g, ""))) {
-        setErrors((prev) => ({ ...prev, price: "Invalid price format" }));
-    } else {
-        setErrors((prev) => {
-            const { price, ...rest } = prev;
-            return rest;
-        });
-    }
-};
+// export const validateInterestRate = (value: string) => {
+//     if (!/^[1-9]\d?(\.\d{1,2})?$/.test(value)) {
+//         setErrors((prev) => ({
+//             ...prev,
+//             interestRate: "Invalid interest rate format",
+//         }));
+//     } else {
+//         setErrors((prev) => {
+//             const { interestRate, ...rest } = prev;
+//             return rest;
+//         });
+//     }
+// };
 
 
-export const handlePriceChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const formattedValue = e.target.value
-        .replace(/\D/g, "")
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    // setPrice(formattedValue);
-    validatePrice(formattedValue);
-};
+
+// export const validatePrice = (value: string) => {
+//     if (!/^[1-9][0-9]{0,11}$/.test(value.replace(/,/g, ""))) {
+//         setErrors((prev) => ({ ...prev, price: "Invalid price format" }));
+//     } else {
+//         setErrors((prev) => {
+//             const { price, ...rest } = prev;
+//             return rest;
+//         });
+//     }
+// };
+
+
+// export const handlePriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const formattedValue = e.target.value
+//         .replace(/\D/g, "")
+//         .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+//     // setPrice(formattedValue);
+//     validatePrice(formattedValue);
+// };
